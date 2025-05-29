ad78d917c7db00ee01f7fa24afd63c29aace76c6
Texarkanine <texarkanine@protonmail.com>
2025-05-29 12:15:05 -0500

    fix: bumped version

--- mdc-render.js
+++ mdc-render.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cursor Rule Markdown Renderer for GitHub
 // @namespace    https://github.com/texarkanine
-// @version      2025-05-27
+// @version      1.2.4
 // @description  Renders Cursor Rules (*.mdc) markdown on GitHub into actual Markdown locally, using the marked library + highlight.js.
 // @author       Texarkanine
 // @match        https://github.com/*
